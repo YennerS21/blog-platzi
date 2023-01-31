7fc85cc45e9fb2e94fc1784b2d9e52399d5ddbe8
YennerS21 <yenner.211999@gmail.com>
2023-01-31 16:35:35 -0500

    Programar el metodo index en en PostController y crear vista index para ver las publicaciones y gestionarlas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -38,5 +39,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::resource('posts', PostController::class)->except('show');
 require __DIR__.'/auth.php';
