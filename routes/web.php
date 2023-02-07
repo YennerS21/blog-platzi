775ed91650215c84d3c4cfbb80454cbfbe07d05b
YennerS21 <yenner.211999@gmail.com>
2023-02-06 20:12:15 -0500

    Instalar paquete para monitorear el correcto funcionamiento del blog y mejoras en la navegacion por el blog

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,7 @@ Route::controller(PageController::class)->group(function ()
     Route::get('/blog/{post:slug}', 'post')->name('post');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('dashboard', 'posts')->middleware(['auth'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::resource('posts', PostController::class)->except('show');
+Route::resource('posts', PostController::class)->middleware(['auth'])->except('show');
 require __DIR__.'/auth.php';
